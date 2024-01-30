8421dcb3f73d0a4deb1bd67a58103b38a653c562
Andrew Hong <andrewh@Andrews-MacBook-Pro.local>
2024-01-29 22:03:12 -0600

    update stuff

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -111,7 +111,7 @@ def states(input):
 
     if len(input) == 2:
         sql1 = "SELECT state FROM states WHERE abbreviation = %s"
-        cur.execute(sql1, [input,])
+        cur.execute(sql1, [input])
         result = cur.fetchone()
 
         sql2 = "SELECT population FROM uscities WHERE state = %s"
